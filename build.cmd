55ac454d94aa6d94de4a081bd49ae7212203ee91
alzneo <25908453+alzneo@users.noreply.github.com>
2023-09-08 01:24:03 +0500

    add linux binary build

--- build.cmd
+++ build.cmd
@@ -1,6 +1,12 @@
+@echo off
+setlocal
 for /f "delims=" %%i in ('git describe --always') do set VERSION=%%i
-set LDFLAGS="-X main.VERSION=%VERSION%"
+set LDFLAGS="-X main.VERSION=%VERSION% -s -w"
 
 set GOOS=windows
 set GOARCH=amd64
 go build -ldflags %LDFLAGS%
+
+set GOOS=linux
+go build -ldflags %LDFLAGS%
+endlocal
